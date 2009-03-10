0592fad83e84d4dea2d05436e46d6e5d90c5f343
Peter van der Does <peter@avirtualhome.com>
2009-03-09 19:37:53 -0500

    Mail to admin of the board

--- inc/avh-stopspam.public.php
+++ inc/avh-stopspam.public.php
@@ -27,15 +27,24 @@ class AVHStopSpamPublic extends AVHStopSpamCore
 
 	function initPublic ()
 	{
+		$time_start = microtime( true );
 		$ip = $_SERVER['REMOTE_ADDR'];
 		$result = $this->handleRESTcall( $this->getRestIPLookup( $ip ) );
-		$info = $this->ConvertXML2Array( $result );
-		if ( 'yes' == $info['appears'] ) {
-			$message =  'Spam IP	' . $ip.'\n';
-			$message .= 'Last Seen	' . $info['lastseen'].'\n';
-			$message .= 'Frequency	' . $info['frequency'];
-			wp_mail( 'pdoes@avirtualhome.com', sprintf( __( '[%s] Spammer detected' ), get_option( 'blogname' ) ), $message );
+		$spaminfo = $this->ConvertXML2Array( $result );
+		$time_end = microtime( true );
+		$time = $time_end - $time_start;
+		if ( 'yes' == $spaminfo['appears'] ) {
+			$this->handleSpammer( $ip, $spaminfo, $time );
 		}
+	}
+
+	function handleSpammer ( $ip, $info, $time )
+	{
+		$message = 'Spam IP	' . $ip . '\n';
+		$message .= 'Last Seen	' . $info['lastseen'] . '\n';
+		$message .= 'Frequency	' . $info['frequency'] . '\n';
+		$message .= 'Call took	' . $time . '\n';
+		wp_mail( get_option( 'admin_email' ), sprintf( __( '[%s] Spammer detected' ), get_option( 'blogname' ) ), $message );
 	}
 }
 ?>
