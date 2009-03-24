ae0dc74ac6b7448a89504ec90aeb6262c3916fea
Peter van der Does <peter@avirtualhome.com>
2009-03-23 21:19:40 -0500

    Release of version 1.1-rc2

--- avh-fdas.client.php
+++ avh-fdas.client.php
@@ -63,7 +63,7 @@ class AVH_FDAS_Core {
 	function __construct ()
 	{
 
-		$this->version = "1.1-rc1";
+		$this->version = "1.1-rc2";
 		$this->comment_begin = '<!-- AVH First Defense Against Spam version ' . $this->version . ' Begin -->';
 		$this->comment_end = '<!-- AVH First Defense Against Spam version ' . $this->version . ' End -->';
 
