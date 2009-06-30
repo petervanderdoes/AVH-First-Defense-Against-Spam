c6c35653adcca29d71f0d012b5bfb5cebb43e829
Peter van der Does <peter@avirtualhome.com>
2009-06-30 13:50:07 -0500

    Display counter statistics in Overview

--- inc/avh-fdas.admin.php
+++ inc/avh-fdas.admin.php
@@ -128,26 +128,32 @@ class AVH_FDAS_Admin
 		echo '<h2>' . __( 'Overview', 'avfdas' ) . '</h2>';
 		echo '<h3>'.__('Statistics','avhfdas').'</h3>';
 		echo '<p>';
-		echo '<table>';
+
+
 		$data = $this->core->getData();
 		$spam_count=$data['counters'];
 		krsort($spam_count);
 		$have_spam_count_data=false;
+		$output='';
 		foreach ($spam_count as $key => $value) {
 			if ('190001' == $key) {
 				continue;
 			}
 			$have_spam_count_data=true;
 			$date=date_i18n('Y - F',mktime(0,0,0,substr($key,4,2),1,substr($key,0,4)));
-			echo '<tr><th>'.__('Period','avhfdas').'</th><th>'.__('Spam Stopped','avhfdas').'</th></tr>';
-			echo '<td>'.$date.'</td>';
-			echo '<td>'.$value.'</td>';
-			echo '</tr>';
+			$output .= '<td>'.$date.'</td>';
+			$output .= '<td>'.$value.'</td>';
+			$output .= '</tr>';
 		}
-		if (!$have_spam_count_data) {
+		if ($have_spam_count_data) {
+			echo '<table>';
+			echo '<tr><th>'.__('Period','avhfdas').'</th><th>'.__('Spam Stopped','avhfdas').'</th></tr>';
+			echo $output;
+			echo '</table>';
+		} else {
 			_e('No statistics yet','avhfdas');
 		}
-		echo '</table>';
+
 		echo '<h3>' . __('General Options','avhfdas') .'</h3>';
 		echo '<p>';
 		echo __('Use Stop Forum Spam','avhfdas').': ';
