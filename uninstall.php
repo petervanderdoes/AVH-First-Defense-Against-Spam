5cb5f684250cd9806abb1c35945a222651621893
Peter van der Does <peter@avirtualhome.com>
2009-10-26 09:21:26 -0500

    Uninstall IP cache table

--- uninstall.php
+++ uninstall.php
@@ -12,6 +12,6 @@ if ( 'avh-first-defense-against-spam' == dirname( $file ))  {
 	delete_option( 'avhfdas_data' );
 	delete_option( 'avhfdas_nonces' );
 	$ipcache = $wpdb->prefix . 'avhfdas_ipcache';
-	$result = $wpdb->get_row( $wpdb->prepare( "DROP TABLE IF EXISTS %s", $ipcache ) );
+	$result = $wpdb->query( 'DROP TABLE IF EXISTS `'. $ipcache.'`' );
 }
 ?>
