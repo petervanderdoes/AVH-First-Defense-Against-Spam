2d5d4f029c82ae27152024ce260fba529a6a73d0
Peter van der Does <peter@avirtualhome.com>
2010-09-02 22:28:49 -0500

    Stop Forum Spam is using serialized returns now :) No more need for XML conversion
    Signed-off-by: Peter van der Does <peter@avirtualhome.com>

--- class/avh-fdas.core.php
+++ class/avh-fdas.core.php
@@ -329,28 +329,13 @@ class AVH_FDAS_Core
 	 */
 	function handleRESTcall ( $query_array )
 	{
-		$xml_array = array ();
 		$querystring = $this->BuildQuery( $query_array );
 		$url = $this->Settings->getSetting( 'stopforumspam_endpoint' ) . '?' . $querystring;
 		// Starting with WordPress 2.7 we'll use the HTTP class.
 		if ( function_exists( 'wp_remote_request' ) ) {
 			$response = wp_remote_request( $url, array ('user-agent' => 'WordPress/AVH ' . $this->Settings->getSetting( 'version' ) . '; ' . get_bloginfo( 'url' ) ) );
 			if ( ! is_wp_error( $response ) ) {
-				$xml_array = $this->ConvertXML2Array( $response['body'] );
-				if ( ! empty( $xml_array ) ) {
-					// Did the call succeed?
-					if ( 'true' == $xml_array['response_attr']['success'] ) {
-						$return_array = $xml_array['response'];
-					} else {
-						if ( isset( $xml_array['response']['error'] ) ) {
-							$return_array = array ('Error' => $xml_array['response']['error'] );
-						} else {
-							$return_array = array ('Error' => 'Unsuccesfull response from SFS', 'Debug' => var_export( $response, true ) . "/n" . var_export( $xml_array, true ) );
-						}
-					}
-				} else {
-					$return_array = array ('Error' => 'Unknown response from stopforumspam', 'Debug' => var_export( $response, true ) . "/n" . var_export( $xml_array, true ) );
-				}
+				$return_array = unserialize($response['body']);
 			} else {
 				$return_array = array ('Error' => $response->errors );
 			}
@@ -410,121 +395,6 @@ class AVH_FDAS_Core
 	}
 
 	/**
-	 * Convert XML into an array
-	 *
-	 * @param string $contents
-	 * @param integer $get_attributes
-	 * @param string $priority
-	 * @return array
-	 * @see http://www.bin-co.com/php/scripts/xml2array/
-	 */
-	function ConvertXML2Array ( $contents = '', $get_attributes = 1, $priority = 'tag' )
-	{
-		$xml_values = '';
-		$return_array = array ();
-		$tag = '';
-		$type = '';
-		$level = 0;
-		$attributes = array ();
-		if ( function_exists( 'xml_parser_create' ) ) {
-			$parser = xml_parser_create( 'UTF-8' );
-			xml_parser_set_option( $parser, XML_OPTION_TARGET_ENCODING, "UTF-8" );
-			xml_parser_set_option( $parser, XML_OPTION_CASE_FOLDING, 0 );
-			xml_parser_set_option( $parser, XML_OPTION_SKIP_WHITE, 1 );
-			xml_parse_into_struct( $parser, trim( $contents ), $xml_values );
-			xml_parser_free( $parser );
-			//Initializations
-			$xml_array = array ();
-			$parent = array ();
-			$current = &$xml_array; // Reference
-			// Go through the tags.
-			$repeated_tag_index = array ();
-			// Multiple tags with same name will be turned into an array
-			foreach ( $xml_values as $data ) {
-				unset( $attributes, $value ); //Remove existing values, or there will be trouble
-				// This command will extract these variables into the foreach scope
-				// tag(string), type(string), level(int), attributes(array).
-				extract( $data ); //We could use the array by itself, but this cooler.
-				$result = array ();
-				$attributes_data = array ();
-				if ( isset( $value ) ) {
-					if ( $priority == 'tag' ) {
-						$result = $value;
-					} else {
-						$result['value'] = $value; //Put the value in an associate array if we are in the 'Attribute' mode
-					}
-				}
-				// Set the attributes too
-				if ( isset( $attributes ) and $get_attributes ) {
-					foreach ( $attributes as $attr => $val ) {
-						if ( $priority == 'tag' ) $attributes_data[$attr] = $val;
-						else
-							$result['attr'][$attr] = $val; //Set all the attributes in a array called 'attr'
-					}
-				}
-				// See tag status and do what's needed
-				if ( $type == "open" ) { // The starting of the tag '<tag>'
-					$parent[$level - 1] = &$current;
-					if ( ! is_array( $current ) or (! in_array( $tag, array_keys( $current ) )) ) { //Insert New tag
-						$current[$tag] = $result;
-						if ( $attributes_data ) $current[$tag . '_attr'] = $attributes_data;
-						$repeated_tag_index[$tag . '_' . $level] = 1;
-						$current = &$current[$tag];
-					} else { // There was another element with the same tag name
-						if ( isset( $current[$tag][0] ) ) { //If there is a 0th element it is already an array
-							$current[$tag][$repeated_tag_index[$tag . '_' . $level]] = $result;
-							$repeated_tag_index[$tag . '_' . $level] ++;
-						} else { //This section will make the value an array if multiple tags with the same name appear together
-							$current[$tag] = array ($current[$tag], $result );
-							//This will combine the existing item and the new item together to make an array
-							$repeated_tag_index[$tag . '_' . $level] = 2;
-							if ( isset( $current[$tag . '_attr'] ) ) { // The attribute of the last(0th) tag must be moved as well
-								$current[$tag]['0_attr'] = $current[$tag . '_attr'];
-								unset( $current[$tag . '_attr'] );
-							}
-						}
-						$last_item_index = $repeated_tag_index[$tag . '_' . $level] - 1;
-						$current = &$current[$tag][$last_item_index];
-					}
-				} elseif ( $type == "complete" ) { //Tags that ends in 1 line '<tag />'
-					//See if the key is already taken.
-					if ( ! isset( $current[$tag] ) ) { // New key
-						$current[$tag] = $result;
-						$repeated_tag_index[$tag . '_' . $level] = 1;
-						if ( $priority == 'tag' and $attributes_data ) $current[$tag . '_attr'] = $attributes_data;
-					} else { //If taken, put all things inside a list(array)
-						if ( isset( $current[$tag][0] ) and is_array( $current[$tag] ) ) {
-							//This will combine the existing item and the new item together to make an array
-							$current[$tag][$repeated_tag_index[$tag . '_' . $level]] = $result;
-							if ( $priority == 'tag' and $get_attributes and $attributes_data ) {
-								$current[$tag][$repeated_tag_index[$tag . '_' . $level] . '_attr'] = $attributes_data;
-							}
-							$repeated_tag_index[$tag . '_' . $level] ++;
-						} else { //If it is not an array...
-							$current[$tag] = array ($current[$tag], $result ); //...Make it an array using using the existing value and the new value
-							$repeated_tag_index[$tag . '_' . $level] = 1;
-							if ( $priority == 'tag' and $get_attributes ) {
-								if ( isset( $current[$tag . '_attr'] ) ) { //The attribute of the last(0th) tag must be moved as well
-									$current[$tag]['0_attr'] = $current[$tag . '_attr'];
-									unset( $current[$tag . '_attr'] );
-								}
-								if ( $attributes_data ) {
-									$current[$tag][$repeated_tag_index[$tag . '_' . $level] . '_attr'] = $attributes_data;
-								}
-							}
-							$repeated_tag_index[$tag . '_' . $level] ++; //0 and 1 index is already taken
-						}
-					}
-				} elseif ( $type == 'close' ) { //End of tag '</tag>'
-					$current = &$parent[$level - 1];
-				}
-			}
-			$return_array = $xml_array;
-		}
-		return ($return_array);
-	}
-
-	/**
 	 * Parameter for Rest Call IP Lookup
 	 *
 	 * @param string $ip
@@ -532,7 +402,7 @@ class AVH_FDAS_Core
 	 */
 	function getRestIPLookup ( $ip )
 	{
-		$iplookup = array ('ip' => $ip );
+		$iplookup = array ('ip' => $ip,'f'=>'serial' );
 		return $iplookup;
 	}
 
