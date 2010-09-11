199a36a94c6407dffcaac09d880185f1722093ed
Peter van der Does <peter@avirtualhome.com>
2010-09-10 21:11:30 -0500

    No need for singlton class here anymore

--- avh-fdas.client.php
+++ avh-fdas.client.php
@@ -3,41 +3,6 @@
 if ( ! defined( 'AVH_FRAMEWORK' ) ) die( 'You are not allowed to call this page directly.' );
 
 /**
- * Singleton Class
- */
-class AVH_FDAS_Singleton
-{
-
-	function &getInstance ( $class, $arg1 = null )
-	{
-		static $instances = array (); // array of instance names
-		if ( array_key_exists( $class, $instances ) ) {
-			$instance = & $instances[$class];
-		} else {
-			if ( ! class_exists( $class ) ) {
-				switch ( $class )
-				{
-					case 'AVH_FDAS_Core' :
-						require_once (dirname( __FILE__ ) . '/class/avh-fdas.core.php');
-						break;
-					case 'AVH_FDAS_Public' :
-						require_once (dirname( __FILE__ ) . '/class/avh-fdas.public.php');
-						break;
-					case 'AVH_FDAS_DB' :
-						require_once (dirname( __FILE__ ) . '/class/avh-fdas.db.php');
-						break;
-
-				}
-			}
-			$instances[$class] = new $class( $arg1 );
-			$instance = & $instances[$class];
-		}
-		return $instance;
-	} // getInstance
-} // singleton
-
-
-/**
  * Initialize the plugin
  *
  */
@@ -62,7 +27,6 @@ function avh_FDAS_init ()
 	$avhfdas_public = new AVH_FDAS_Public();
 
 } // End avh_FDAS__init()
-
 
 add_action( 'plugins_loaded', 'avh_FDAS_init' );
 ?>
