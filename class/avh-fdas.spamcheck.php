56a792219299af7529f10df01fa6a041d662df60
Peter van der Does <peter@avirtualhome.com>
2010-09-22 11:50:46 -0500

    Missed class call

--- class/avh-fdas.spamcheck.php
+++ class/avh-fdas.spamcheck.php
@@ -219,7 +219,7 @@ class AVH_FDAS_SpamCheck
 	public function checkWhitelist ()
 	{
 		if ( $this->_core_options['general']['usewhitelist'] ) {
-			$found = $this->_checkList( getDataElement('lists','whitelist') );
+			$found = $this->_checkList( $this->_core->getDataElement('lists','whitelist') );
 			if ( $found ) {
 				$this->ip_in_white_list = true;
 			}
