5f8f703b49b4aa8d410449782a6bf33e883e80b1
Peter van der Does <peter@avirtualhome.com>
2011-04-05 08:12:05 -0400

    Nonce filed needed for ajax call

--- class/avh-fdas.ipcachelist.php
+++ class/avh-fdas.ipcachelist.php
@@ -300,8 +300,8 @@ class AVH_FDAS_IPCacheList extends WP_List_Table {
 			$class_ham='delete:the-ipcache-list:'.$ip->ip.':ham:e7e7d3:e7e7d3:new=spam vim-u';
 			$class_spam='delete:the-ipcache-list:'.$ip->ip.':spam:e7e7d3:e7e7d3:new=ham vim-a';
 		} else {
-			$actions['spam'] = "<a href='#' class='dim:the-ipcache-list:ip-$ip->ip:spammed:e7e7d3:e7e7d3:new=avhfdas_is_ham vim-a' title='" . esc_attr__('Mark this IP as spam', 'avh-fdas') . "'>" . __('Spam', 'avh-fdas') . '</a>';
-			$actions['ham'] = "<a href='#' class='dim:the-ipcache-list:ip-$ip->ip:spammed:e7e7d3:e7e7d3:new=avhfdas_is_spam vim-u' title='" . esc_attr__('Mark this IP as ham', 'avh-fdas') . "'>" . __('Ham', 'avh-fdas') . '</a>';
+			$actions['spam'] = "<a href='#?$hamspam_nonce' class='dim:the-ipcache-list:ip-$ip->ip:spammed:e7e7d3:e7e7d3:new=avhfdas_is_ham vim-a' title='" . esc_attr__('Mark this IP as spam', 'avh-fdas') . "'>" . __('Spam', 'avh-fdas') . '</a>';
+			$actions['ham'] = "<a href='#?$hamspam_nonce' class='dim:the-ipcache-list:ip-$ip->ip:spammed:e7e7d3:e7e7d3:new=avhfdas_is_spam vim-u' title='" . esc_attr__('Mark this IP as ham', 'avh-fdas') . "'>" . __('Ham', 'avh-fdas') . '</a>';
 		};
 				
 		$actions['blacklist'] = "<a href='#' class='' title='" . esc_attr__('Blacklist this IP', 'avh-fdas') . "'>" . __('Blacklist', 'avh-fdas') . '</a>';
