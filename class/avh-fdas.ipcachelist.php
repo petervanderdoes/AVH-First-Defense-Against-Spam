25fee4e5a41d59aedb34caeb781a92e3158814a6
Peter van der Does <peter@avirtualhome.com>
2011-04-05 16:23:04 -0400

    Better URLS for ham/spam

--- class/avh-fdas.ipcachelist.php
+++ class/avh-fdas.ipcachelist.php
@@ -288,20 +288,20 @@ class AVH_FDAS_IPCacheList extends WP_List_Table {
 		$del_nonce = esc_html('_wpnonce=' . wp_create_nonce("delete-ip_$ip->ip"));
 		$hamspam_nonce = esc_html('_wpnonce=' . wp_create_nonce("hamspam-ip_$ip->ip"));
 		
-		#$url = "comment.php?c=$comment->comment_ID";
+		$url = "http://blog.grandprix.int/wp-admin/admin.php?page=avh-first-defense-against-spam-ip-cache-log";
 		
-		$ham_url = esc_url($url . "&action=hamip&$hamspam_nonce");
-		$spam_url = esc_url($url . "&action=spamip&hamspam_nonce");
-		$delete_url = esc_url($url . "&action=deleteip&$del_nonce");
+		$ham_url = esc_url($url."&action=hamip&$hamspam_nonce");
+		$spam_url = esc_url($url."&action=spamip&hamspam_nonce");
+		$delete_url = esc_url($url."&action=deleteip&$del_nonce");
 		
 		$actions = array('ham'=>'', 'spam'=>'', 'blacklist'=>'', 'delete'=>'');
 		
 		if ( $ip_status && 'all' != $ip_status ) { // not looking at all comments
-			$actions['spam']="<a href='#?$hamspam_nonce' class='delete:the-ipcache-list:ip-$ip->ip:spammed:e7e7d3:e7e7d3:new_status=1 vim-a vim-destructive' title='" . esc_attr__('Mark this IP as spam', 'avh-fdas') . "'>" . __('Spam', 'avh-fdas') . '</a>';
-			$actions['ham'] = "<a href='#?$hamspam_nonce' class='delete:the-ipcache-list:ip-$ip->ip:spammed:e7e7d3:e7e7d3:new_status=0 vim-u vim-destructive' title='" . esc_attr__('Mark this IP as ham', 'avh-fdas') . "'>" . __('Ham', 'avh-fdas') . '</a>';
+			$actions['spam']="<a href='$spam_url' class='delete:the-ipcache-list:ip-$ip->ip:spammed:e7e7d3:e7e7d3:new_status=1 vim-a vim-destructive' title='" . esc_attr__('Mark this IP as spam', 'avh-fdas') . "'>" . __('Spam', 'avh-fdas') . '</a>';
+			$actions['ham'] = "<a href='$ham_url' class='delete:the-ipcache-list:ip-$ip->ip:spammed:e7e7d3:e7e7d3:new_status=0 vim-u vim-destructive' title='" . esc_attr__('Mark this IP as ham', 'avh-fdas') . "'>" . __('Ham', 'avh-fdas') . '</a>';
 		} else {
-			$actions['spam'] = "<a href='#?$hamspam_nonce' class='dim:the-ipcache-list:ip-$ip->ip:spammed:e7e7d3:e7e7d3:new_status=1 vim-a' title='" . esc_attr__('Mark this IP as spam', 'avh-fdas') . "'>" . __('Spam', 'avh-fdas') . '</a>';
-			$actions['ham'] = "<a href='#?$hamspam_nonce' class='dim:the-ipcache-list:ip-$ip->ip:spammed:e7e7d3:e7e7d3:new_status=0 vim-u' title='" . esc_attr__('Mark this IP as ham', 'avh-fdas') . "'>" . __('Ham', 'avh-fdas') . '</a>';
+			$actions['spam'] = "<a href='$spam_url' class='dim:the-ipcache-list:ip-$ip->ip:spammed:e7e7d3:e7e7d3:new_status=1 vim-a' title='" . esc_attr__('Mark this IP as spam', 'avh-fdas') . "'>" . __('Spam', 'avh-fdas') . '</a>';
+			$actions['ham'] = "<a href='$ham_url' class='dim:the-ipcache-list:ip-$ip->ip:spammed:e7e7d3:e7e7d3:new_status=0 vim-u' title='" . esc_attr__('Mark this IP as ham', 'avh-fdas') . "'>" . __('Ham', 'avh-fdas') . '</a>';
 		};
 				
 		$actions['blacklist'] = "<a href='#' class='' title='" . esc_attr__('Blacklist this IP', 'avh-fdas') . "'>" . __('Blacklist', 'avh-fdas') . '</a>';
