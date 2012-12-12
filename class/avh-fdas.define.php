61d78b3c0a8f4c2a80ce4a6257b42b01a76d0732
Peter van der Does <github@avirtualhome.com>
2012-12-12 07:55:09 -0500

    Version bump 3.5.1

--- class/avh-fdas.define.php
+++ class/avh-fdas.define.php
@@ -7,7 +7,7 @@ final class AVH_FDAS_Define
 	/**
 	 * General Constants
 	 */
-	const PLUGIN_VERSION = '3.5.1-rc.1';
+	const PLUGIN_VERSION = '3.5.1';
 	const PLUGIN_README_URL = 'http://svn.wp-plugins.org/avh-first-defense-against-spam/trunk/readme.txt';
 	const PLUGIN_PATH = 'avh-first-defense-against-spam';
 	const PLUGIN_FILE = 'avh-first-defense-against-spam/avh-fdas.php';
